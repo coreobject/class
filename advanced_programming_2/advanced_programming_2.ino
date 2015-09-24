842676370d3b649b32cbab752b685c94a9a3936d
Ariel Churi <git@sparklelabs.com>
2015-09-24 18:14:43 -0400

    created advanced 2

---
+++ advanced_programming_2/advanced_programming_2.ino
@@ -0,0 +1,76 @@
+//create variables for pins
+const int buttonPin = 2;
+const int ledPin =  12;
+const int fadeLed = 11;
+const int potPin = 0;
+const int lightPin = 1;
+
+// Time code
+unsigned long previousMillis = 0;
+const long interval = 10;
+
+// LED fading variables
+boolean fadeUp = true;
+int fadeLevel = 0;
+//LED blinking variables
+int ledState = LOW;
+//button variables
+int buttonState = 0;
+boolean pressed = false;
+//analog sensor variables
+int lightVar = 0;
+int potVar = 0;
+
+//state machine variables
+int stateVar;
+
+void setup() {
+  // set up pins
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(fadeLed, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+ buttonState = digitalRead(buttonPin);
+
+  switch (stateVar) {
+    case 0:
+    Serial.println("night light mode");
+      //check sensors
+      potVar = analogRead(potPin);
+      lightVar = analogRead(lightPin);
+
+      //logic
+      if ( lightVar <= potVar ) {
+        fadeUp = true;
+      } else {
+        fadeUp = false;
+      }
+
+      //display
+      if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        // Serial.println();
+        if (fadeUp == true) {
+          if (fadeLevel != 255) fadeLevel++;
+        } else {
+          if (fadeLevel != 0) fadeLevel--;
+        }
+      }
+      break;
+    case 1:
+Serial.println("projector mode");
+      break;
+  }
+
+
+  //write to the LED pins
+  digitalWrite(ledPin, ledState);
+  analogWrite(fadeLed, fadeLevel);
+
+  
+}
